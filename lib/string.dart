74516bc3573d9f99963efee28303db564a94ae76
gustiarifiyanto <gusti.arifiyanto@gmail.com>
2025-12-22 12:41:09 +0700

    menambahkan beberapa perintah type data

--- lib/string.dart
+++ lib/string.dart
@@ -1,4 +1,56 @@
 void main() {
-  String nama = 'apa kabar';
-  print(nama);
+  //var (type data yang tidak bisa diganti)
+  // var data = 'ini data var';
+  // data = 'ini sama aja';
+  // print (data);
+  
+  //object (type data yang bisa diganti)
+  // Object data ='ini data object';
+  // data = 900;
+  // print(data);
+  
+  //dynamic (type data yang bisa diganti)
+  // dynamic data = 'ini data dynamic';
+  // data = 300;
+  // print(data);
+
+  //map string int (typa data yang bisa di gabung)
+  // Map<String, int> bebas = {
+  //   'satu':1,
+  //   'dua':2,
+  //   'tiga':3
+  // };
+  // print(bebas);
+  
+  //list string (typa data yang berurutan, bisa string maupun int)
+  // List <String> nama = ['gusti','mia','budi'];
+  // print(nama);
+  
+  //list int (type daya yang berurutan, hanya int saja)
+  // List data = [1,2,3,4,5];
+  // print(data);
+  
+  //boolean (type data true or false)
+  // bool istrue = true;
+  // print(istrue);
+  // if (istrue){
+  //   print ('benar');
+  // // ignore: dead_code
+  // }else {
+  //   print('salah');
+  // }
+
+  //String (type data text)
+  // String nama = 'apa kabar';
+  // print(nama);
+
+  //Integer (type data angka bulat)
+  // num data = 33;
+  // print(data);
+  // data = 90.90;
+  // print(data);
+
+  //Double (type data angka desimal)
+  // double myd = 2.33;
+  // print(myd);
 }
