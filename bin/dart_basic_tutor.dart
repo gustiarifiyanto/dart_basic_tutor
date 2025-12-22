48e06adf88723699703220e202f4d4a41dad7fff
gustiarifiyanto <gusti.arifiyanto@gmail.com>
2025-12-22 12:10:03 +0700

    Git initial

---
+++ bin/dart_basic_tutor.dart
@@ -0,0 +1,5 @@
+import 'package:dart_basic_tutor/dart_basic_tutor.dart' as dart_basic_tutor;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_basic_tutor.calculate()}!');
+}
---
+++ test/dart_basic_tutor_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_basic_tutor/dart_basic_tutor.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
