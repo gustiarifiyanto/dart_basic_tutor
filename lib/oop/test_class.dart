5902d65c3d1759941d0cf52bfc657d7cb8ab4137
gustiarifiyanto <gusti.arifiyanto@gmail.com>
2025-12-23 14:23:40 +0700

    masih ada problem saat run test pada void main

---
+++ lib/oop/test_class.dart
@@ -0,0 +1,14 @@
+import 'package:dart_basic_tutor/oop/mobil.dart';
+
+
+void main() {
+  Mobil mobil = Mobil();
+  mobil.merk = 'Toyota';
+  mobil.model = 'Avanza';
+  mobil.tahun = 2020;
+
+  Mobil mobil2 = Mobil();
+  mobil2.merk = 'Honda';
+  mobil2.model = 'Civic';
+  mobil2.tahun = 2021;
+}
